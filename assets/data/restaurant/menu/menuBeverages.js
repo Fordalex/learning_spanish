a1042aeaa3953d22cf17104f2df4cbdc415b7188
Alex Ford <alex96ford19@gmail.com>
2023-11-16 22:46:34 +0000

    Add some more data

---
+++ assets/data/restaurant/menu/menuBeverages.js
@@ -0,0 +1,164 @@
+const menuBeverages = [
+  {
+    "english": "Coffee",
+    "spanish": "Café",
+    "gender": "masculine"
+  },
+  {
+    "english": "Tea",
+    "spanish": "Té",
+    "gender": "masculine"
+  },
+  {
+    "english": "Espresso",
+    "spanish": "Espresso",
+    "gender": "masculine"
+  },
+  {
+    "english": "Cappuccino",
+    "spanish": "Capuchino",
+    "gender": "masculine"
+  },
+  {
+    "english": "Latte",
+    "spanish": "Latte",
+    "gender": "masculine"
+  },
+  {
+    "english": "Hot Chocolate",
+    "spanish": "Chocolate Caliente",
+    "gender": "masculine"
+  },
+  {
+    "english": "Iced Tea",
+    "spanish": "Té Helado",
+    "gender": "masculine"
+  },
+  {
+    "english": "Lemonade",
+    "spanish": "Limonada",
+    "gender": "feminine"
+  },
+  {
+    "english": "Soda",
+    "spanish": "Refresco",
+    "gender": "masculine"
+  },
+  {
+    "english": "Mineral Water",
+    "spanish": "Agua Mineral",
+    "gender": "feminine"
+  },
+  {
+    "english": "Smoothie",
+    "spanish": "Batido",
+    "gender": "masculine"
+  },
+  {
+    "english": "Milkshake",
+    "spanish": "Malteada",
+    "gender": "feminine"
+  },
+  {
+    "english": "Beer",
+    "spanish": "Cerveza",
+    "gender": "feminine"
+  },
+  {
+    "english": "Wine",
+    "spanish": "Vino",
+    "gender": "masculine"
+  },
+  {
+    "english": "Cocktail",
+    "spanish": "Cóctel",
+    "gender": "masculine"
+  },
+  {
+    "english": "Martini",
+    "spanish": "Martini",
+    "gender": "masculine"
+  },
+  {
+    "english": "Whiskey",
+    "spanish": "Whisky",
+    "gender": "masculine"
+  },
+  {
+    "english": "Gin",
+    "spanish": "Ginebra",
+    "gender": "feminine"
+  },
+  {
+    "english": "Vodka",
+    "spanish": "Vodka",
+    "gender": "masculine"
+  },
+  {
+    "english": "Rum",
+    "spanish": "Ron",
+    "gender": "masculine"
+  },
+  {
+    "english": "Orange Dilute",
+    "spanish": "Diluido de Naranja",
+    "gender": "masculine"
+  },
+  {
+    "english": "Ribena",
+    "spanish": "Ribena",
+    "gender": "feminine"
+  },
+  {
+    "english": "Milk",
+    "spanish": "Leche",
+    "gender": "feminine"
+  },
+  {
+    "english": "Fruit Juice",
+    "spanish": "Jugo de Frutas",
+    "gender": "masculine"
+  },
+  {
+    "english": "Iced Coffee",
+    "spanish": "Café Helado",
+    "gender": "masculine"
+  },
+  {
+    "english": "Green Tea",
+    "spanish": "Té Verde",
+    "gender": "masculine"
+  },
+  {
+    "english": "Herbal Tea",
+    "spanish": "Té de Hierbas",
+    "gender": "masculine"
+  },
+  {
+    "english": "Sparkling Water",
+    "spanish": "Agua con Gas",
+    "gender": "feminine"
+  },
+  {
+    "english": "Apple Cider",
+    "spanish": "Sidra de Manzana",
+    "gender": "feminine"
+  },
+  {
+    "english": "Energy Drink",
+    "spanish": "Bebida Energética",
+    "gender": "feminine"
+  },
+  {
+    "english": "Hot Cider",
+    "spanish": "Sidra Caliente",
+    "gender": "feminine"
+  },
+  {
+    "english": "Chocolate Milk",
+    "spanish": "Leche de Chocolate",
+    "gender": "feminine"
+  }
+];
+
+window.menuBeverages = menuBeverages;
